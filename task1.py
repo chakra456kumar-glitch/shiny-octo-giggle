8486f09e30cd4ce2507ec56e15895f5dfbbd20ba
chakra456kumar-glitch <chakra456.kumar@gmail.com>
2025-09-07 17:43:47 +0530

    Add Python script
    Add two file

---
+++ task1.py
@@ -0,0 +1,14 @@
+n=int(input("Enter first number:"))
+n1=int(input("Enter second number: "))
+add=n+n1
+print("Addition", add)
+sub=n-n1
+print("Subtraction", sub)
+mul=n*n1
+print("Multiplication", mul)
+div=n/n1
+print("Division", div)
+
+
+
+      
---
+++ task2.py
@@ -0,0 +1,5 @@
+s=input("Enter First Name:   ")
+s1=input("Enter Second Name:   ")
+full_name= s+" "+s1
+
+print("Hello, "+full_name+"!"+"  Welcome to the Python program")
